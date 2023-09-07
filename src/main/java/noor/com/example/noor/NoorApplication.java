61c872da55e573568ca3073366d2c9043477aa76
Kaipulla <roughitsrinivasan17@gmail.com>
2023-09-07 13:35:18 +0530

    first commit

---
+++ src/main/java/noor/com/example/noor/NoorApplication.java
@@ -0,0 +1,13 @@
+package noor.com.example.noor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noor/com/example/noor/NoorApplicationTests.java
@@ -0,0 +1,13 @@
+package noor.com.example.noor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
